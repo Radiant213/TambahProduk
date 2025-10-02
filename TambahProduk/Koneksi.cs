01fd061c6cb488ce8f2f6e9b54214eaf852c2f6a
Radiant213 <galangmaruff1@gmail.com>
2025-10-02 08:20:35 +0700

    Add files via upload

---
+++ TambahProduk/Koneksi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoneksiDatabase
+{
+    internal class Koneksi
+    {
+        public static string connectionString = @"Data Source=RPL2-C1\SQLEXPRESS;Initial Catalog=TokoDR;Integrated Security=True;Encrypt=False;";
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
